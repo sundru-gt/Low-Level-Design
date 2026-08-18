6a4dc5ed32bc6957d9661a105fbed4589fc733ae
sundru <shivendrupaul@gmail.com>
2026-08-18 14:06:43 +0530

    added codes

---
+++ 8StrategyDesignPattern.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+class WalkableRobot {
+public:
+    virtual void walk() = 0;
+    virtual ~WalkableRobot() {}
+};
+
+//Concrete Strategies for walk
+class NormalWalk : public WalkableRobot {
+public:
+    void walk() override { 
+        cout << "Walking normally..." << endl; 
+    }
+};
+
+class NoWalk : public WalkableRobot {
+public:
+    void walk() override { 
+        cout << "Cannot walk." << endl; 
+    }
+};
+
+
+class TalkableRobot {
+public:
+    virtual void talk() = 0;
+    virtual ~TalkableRobot() {}
+};
+
+//Concrete Strategies for Talk
+class NormalTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Talking normally..." << endl; 
+    }
+};
+
+class NoTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Cannot talk." << endl; 
+    }
+};
+
+class FlyableRobot {
+public:
+    virtual void fly() = 0;
+    virtual ~FlyableRobot() {}
+};
+
+class NormalFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Flying normally..." << endl; 
+    }
+};
+
+class NoFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Cannot fly." << endl; 
+    }
+};
+
+//Robot Base Class
+class Robot {
+protected:
+    WalkableRobot* walkBehavior;
+    TalkableRobot* talkBehavior;
+    FlyableRobot* flyBehavior;
+
+public:
+    Robot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f) {
+        this->walkBehavior = w;
+        this->talkBehavior = t;
+        this->flyBehavior = f;
+    }
+       
+    void walk() { 
+        walkBehavior->walk(); 
+    }
+    void talk() { 
+        talkBehavior->talk(); 
+    }
+    void fly() { 
+        flyBehavior->fly(); 
+    }
+
+    virtual void projection() = 0; // Abstract method for subclasses
+};
+
+//Concrete Robot Type
+class CompanionRobot : public Robot {
+public:
+    CompanionRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying friendly companion features..." << endl;
+    }
+};
+
+class WorkerRobot : public Robot {
+public:
+    WorkerRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying worker efficiency stats..." << endl;
+    }
+};
+
+int main() {
+    Robot *robot1 = new CompanionRobot(new NormalWalk(), new NormalTalk(), new NoFly());
+    robot1->walk();
+    robot1->talk();
+    robot1->fly();
+    robot1->projection();
+
+    cout << "--------------------" << endl;
+
+    Robot *robot2 = new WorkerRobot(new NoWalk(), new NoTalk(), new NormalFly());
+    robot2->walk();
+    robot2->talk();
+    robot2->fly();
+    robot2->projection();
+
+    return 0;
+}
