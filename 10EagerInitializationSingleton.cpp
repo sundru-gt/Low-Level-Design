95eb991c112200564d5cef4743b63a3cc65711b0
sundru <shivendrupaul@gmail.com>
2026-08-20 14:50:47 +0530

    added codes

---
+++ 10EagerInitializationSingleton.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class Singleton {
+private:
+    static Singleton* instance;
+
+    Singleton() {
+        cout << "Singleton Constructor called" << endl;
+    }
+
+public:
+    static Singleton* getInstance() {
+        return instance;
+    }
+};
+
+Singleton* Singleton::instance = new Singleton();
+
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) << endl;
+}
